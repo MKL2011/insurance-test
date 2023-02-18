86be4149d21079e7787e7f6a295272a3266a7ba0
kallel.med.amine@gmail.com <mohamedaminekallel@Mohameds-MacBook-Pro.local>
2023-02-18 06:56:32 +0100

    Initial commit

---
+++ src/test/java/com/tinubu/test/InsuranceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinubu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
